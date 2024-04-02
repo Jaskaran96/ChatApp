c6011e08d4a98e3981ea9927d9b52c7ed6115bd3
Jaskaran Singh <bartychuck@gmail.com>
2024-04-03 05:27:41 +0530

    complete basic functionalities

--- sender.js
+++ sender.js
@@ -1,18 +1,43 @@
 const net = require("net");
+const { argv } = require("process");
 const readline = require("readline");
+
+const userName = argv[2].split("=")[1];
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: `${userName} > `,
 });
 
 const socket = net.createConnection({ host: "127.0.0.1", port: 3000 }, () => {
-  console.log("connected to server!");
+  console.log("Connected to server!");
+
+  const obj = { type: "join", user: userName };
+  socket.write(encodedMessage(obj));
+
   rl.on("line", (input) => {
-    socket.write(input);
+    const obj = { type: "msg", user: userName, msg: input };
+    socket.write(encodedMessage(obj));
+    rl.prompt();
   });
 });
 
 socket.on("data", (data) => {
-  console.log("data received: ", data.toString("utf-8"));
+  const decodedData = JSON.parse(data.toString("utf-8"));
+  let prompt = decodedData.user ? `${decodedData.user} > ` : "Server > ";
+  rl.setPrompt(prompt);
+  rl.prompt();
+  console.log(decodedData.message);
+  rl.setPrompt(`${userName} > `);
+  rl.prompt();
 });
+
+socket.on("end", () => {
+  console.log("Disconnected from server!");
+  rl.close();
+});
+
+function encodedMessage(obj) {
+  return JSON.stringify(obj);
+}
--- server.js
+++ server.js
@@ -1,19 +1,84 @@
 //This is the lowest level networking we are able to do with node.
 const net = require("net");
+const PORT = 3000;
+const HOST_ADDR = "127.0.0.1";
+let connectedClients = [];
 
-//The socket is a duplex stream, which means that it can read and write data.
-const connectedClients = [];
-const server = net.createServer((client) => {
-  client.on("data", (data) => {
-    console.log("Data :  ", data.toString("utf-8"));
+//Create a new server instance
+const server = net.createServer();
+
+//Fired whenever a new connection is made to the server
+server.on("connection", (client) => {
+  //The client is a duplex stream, which means that it can read and write data.
+  server.getConnections((err, count) => {
+    const serverMessage = generateServerMessage(
+      "Server",
+      `Welcome to the chat room! Connected Users : ${count}`,
+      null
+    );
+    client.write(JSON.stringify(serverMessage));
   });
-  client.write("Hello from server!");
-  connectedClients.push(client);
 
-  //client.pipe("Hello from server!");
+  client.on("data", (data) => {
+    console.log("Data : ", data.toString("utf-8"));
+    const decodedData = JSON.parse(data.toString("utf-8"));
+    parseMessage(decodedData, client);
+  });
+
+  client.on("close", () => {
+    let userName;
+    connectedClients = connectedClients.filter((clientObj) => {
+      if (clientObj["client"] == client) userName = clientObj["user"];
+      return clientObj["client"] != client;
+    });
+    parseMessage({ type: "leave", user: userName }, client);
+  });
 });
 
-server.listen(3000, "127.0.0.1", () => {
+//Bind the server and start listening on a particular port and host address
+server.listen(PORT, HOST_ADDR, () => {
   console.log("Server is running on port 3000");
   console.log(server.address());
 });
+
+function broadCastData(data) {
+  const encodedMessage = JSON.stringify(data);
+  connectedClients.forEach((clientObj) => {
+    if (clientObj["user"] != data.user) {
+      const clientSocket = clientObj["client"];
+      clientSocket.write(encodedMessage);
+    }
+  });
+}
+
+function parseMessage(data, client) {
+  if (data["type"] == "join") {
+    connectedClients.push({ user: data["user"], client: client });
+
+    const serverMessage = generateServerMessage(
+      "Server",
+      `${data["user"]} has joined the chat room!`,
+      null
+    );
+
+    broadCastData(serverMessage);
+  } else if (data["type"] == "msg") {
+    const serverMessage = generateServerMessage(
+      "User",
+      data["msg"],
+      data["user"]
+    );
+    broadCastData(serverMessage);
+  } else if (data["type"] == "leave") {
+    const serverMessage = generateServerMessage(
+      "Server",
+      `${data["user"]} has left the chat room!`,
+      null
+    );
+    broadCastData(serverMessage);
+  }
+}
+
+function generateServerMessage(type, message, user) {
+  return { type, message, user };
+}
